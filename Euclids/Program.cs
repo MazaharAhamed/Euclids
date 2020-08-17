408ee8f8ebef2884f7d645066734346d031d50e3
MazaharAhamed <mazaharahamed77@gmail.com>
2020-08-17 19:40:13 +0530

    Add project files.

---
+++ Euclids/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Euclids
+{
+    class Program
+    {
+        public static int GCD(int a,int b)
+        {
+            if (b == 0)
+                return a;
+            return GCD(b, a % b);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter values for a and b: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("GCD of " + a + " and " + b + " is: " + GCD(a, b));
+        }
+    }
+}
